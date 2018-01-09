5566bc4c0184561386e631a796453398f730a782
HrafnkellTorri <Kallinnhannhrabbi@hotmail.com>
2018-01-09 15:27:00 +0000

    Add files via upload

---
+++ CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject Target;
+    Vector3 offset;
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - Target.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Target.transform.position + offset;
+    }
+}
---
+++ Player_Movements.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movements : MonoBehaviour {
+
+        public Rigidbody rb;
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            rb.constraints = RigidbodyConstraints.FreezeRotationX;
+    }
+        void FixedUpdate()
+        {
+            float x,z;
+
+
+            x = Input.GetAxis("Horizontal") * Time.deltaTime * 5;
+            z = Input.GetAxis("Vertical") * Time.deltaTime * 5;
+
+            
+
+            if(Physics.Raycast(transform.position,new Vector3(0, -1), 0.6f))
+            {
+            Debug.Log("HIT!");
+                if (Input.GetButtonDown("Jump"))
+                {
+                    rb.velocity = new Vector3(0, 4, 0);
+                }
+                else
+                {
+
+                }
+            }
+
+            rb.AddTorque(z, -1000, x * Time.deltaTime);
+
+
+        // rb.freezeRotation = true;
+    }
+    }
